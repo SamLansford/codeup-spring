5d0cce091312a907346541be2c613cb1226ca1d4
Sam Lansford <sam.j.lansford@gmail.com>
2021-05-03 12:13:38 -0500

    doing views exercises in spring boot

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,18 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
-    public String landing() {
-        return "This is the landing page";
-    }
-
-    @GetMapping("/home")
     public String welcome() {
-        return "hello";
+        return "home";
     }
 }
--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,21 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class PostController {
 
     @RequestMapping(path = "/post", method = RequestMethod.GET)
-    @ResponseBody
-    public String post() {
-        return "posts index page";
+    public String post(Model vModel) {
+        List<Post> posts = new ArrayList<>(Arrays.asList(
+                new Post("Post Title 1", "something 1"),
+                new Post("Post Title 2", "something 2"),
+                new Post("Post Title 3", "something 3")
+        ));
+        vModel.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String postsID(@PathVariable int id) {
-        return "view an individual post " + id;
+    public String postsID(@PathVariable Long id, Model vModel) {
+        Post post = new Post("test Title", "Test Body");
+        vModel.addAttribute("id",id);
+        vModel.addAttribute("post", post);
+        return "posts/show";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
